921447f5500ead9c9309f086dcc2776d9901ca4c
alessiocoser <alessio.coser@xpeppers.com>
2017-01-28 16:34:46 +0100

    adds temperature sensor class

---
+++ test/tempearture-sensor-test.js
@@ -0,0 +1,20 @@
+const assert = require('assert')
+const TemperatureSensor = require('../lib/temperature-sensor')
+
+describe('TemperatureSensor', function () {
+  var sensor = new TemperatureSensor()
+
+  describe('temperature method', function() {
+    it('should return a number', function() {
+      assert(typeof sensor.temperature() == "number")
+    })
+
+    it('should be greater than -10', function() {
+      assert(sensor.temperature() > -10)
+    })
+
+    it('should be lower than 60', function() {
+      assert(sensor.temperature() < 60)
+    })
+  })
+})

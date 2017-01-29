d3235ae0ac1002e166660b2cd67eb61cde070967
alessiocoser <alessio.coser@xpeppers.com>
2017-01-29 14:11:20 +0100

    add schedules all() test

--- test/schedules-test.js
+++ test/schedules-test.js
@@ -1,14 +1,33 @@
 const assert = require('assert')
-const Schedules = require('../lib/schedules')
+const Schedule = require('../lib/schedule')
+const Schedules  = require('../lib/schedules')
 const LowDB = require('lowdb')
-const db = LowDB()
+var db = LowDB()
 
 describe('Schedules', function () {
-  const schedules = new Schedules(db)
 
   describe('all', function () {
     it('should return an empty array without elements', function() {
+      db.setState({schedules: []})
+      var schedules = new Schedules(db)
 
+      assert.deepEqual(schedules.all(), [])
+    })
+
+    it('should return an array of schedule', function() {
+      var obj = {id: 1, temperature: 19, fromTime: '09:00', toTime: '12:00', days: 'week'}
+      db.setState({schedules: [obj]})
+      var expectedSchedule = new Schedule(obj)
+
+      var schedules = new Schedules(db)
+      assert.deepEqual(schedules.all(), [expectedSchedule])
+    })
+
+    it('should return an array of valid schedule', function() {
+      var invalid = {id: 1, temperature: 19}
+      db.setState({schedules: [invalid]})
+
+      var schedules = new Schedules(db)
       assert.deepEqual(schedules.all(), [])
     })
   })

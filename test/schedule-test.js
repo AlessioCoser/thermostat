bedb64aa218b703e9c08b566a18cfc021df62910
alessiocoser <alessio.coser@xpeppers.com>
2017-01-29 13:00:00 +0100

    test schedule dayOption

--- test/schedule-test.js
+++ test/schedule-test.js
@@ -21,16 +21,32 @@ describe('Schedule', function () {
     assert(!schedule.isValid())
   })
 
-  it('should be not valid with incorrect fromTime', function() {
-    var tooMuchHours = new Schedule(19.3, '24:00', '12:00', 'week')
-    var tooMuchMinutes = new Schedule(19.3, '12:00', '18:60', 'week')
+  it('should be not valid with incorrect hours of fromTime', function() {
+    var schedule = new Schedule(19.3, '24:00', '12:00', 'week')
 
-    assert(!tooMuchHours.isValid())
-    assert(!tooMuchMinutes.isValid())
+    assert(!schedule.isValid())
+  })
+
+  it('should be not valid with incorrect minutes of toTime', function() {
+    var schedule = new Schedule(19.3, '12:00', '18:60', 'week')
+
+    assert(!schedule.isValid())
   })
 
   it('should be not valid with fromTime greater than toTime', function() {
     var schedule = new Schedule(19.3, '23:00', '12:00', 'week')
+
+    assert(!schedule.isValid())
+  })
+
+  it('should be not valid with fromTime equals toTime', function() {
+    var schedule = new Schedule(19.3, '23:00', '23:00', 'week')
+
+    assert(!schedule.isValid())
+  })
+
+  it('should be not valid with days not in [week, saturday, sunday]', function() {
+    var schedule = new Schedule(19.3, '09:00', '18:00', 'inexistent')
 
     assert(!schedule.isValid())
   })

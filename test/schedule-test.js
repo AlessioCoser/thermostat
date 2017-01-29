086995f81b236213da1e2f6e3e42c63a5d264aa1
alessiocoser <alessio.coser@xpeppers.com>
2017-01-29 13:15:08 +0100

    change construction parameters and fix tests

--- test/schedule-test.js
+++ test/schedule-test.js
@@ -10,43 +10,50 @@ describe('Schedule', function () {
   })
 
   it('should be valid with correct parameters', function() {
-    var schedule = new Schedule(19.2, '09:00', '12:00', 'week')
+    var opt = {temperature: 19.2, fromTime: '09:00', toTime: '12:00', 'days': 'week'}
+    var schedule = new Schedule(opt)
 
     assert(schedule.isValid())
   })
 
   it('should be not valid with incorrect temperature', function() {
-    var schedule = new Schedule('asd', '09:00', '12:00', 'week')
+    var opt = {temperature: 'asd', fromTime: '09:00', toTime: '12:00', 'days': 'week'}
+    var schedule = new Schedule(opt)
 
     assert(!schedule.isValid())
   })
 
   it('should be not valid with incorrect hours of fromTime', function() {
-    var schedule = new Schedule(19.3, '24:00', '12:00', 'week')
+    var opt = {temperature: 19.3, fromTime: '24:00', toTime: '12:00', 'days': 'week'}
+    var schedule = new Schedule(opt)
 
     assert(!schedule.isValid())
   })
 
   it('should be not valid with incorrect minutes of toTime', function() {
-    var schedule = new Schedule(19.3, '12:00', '18:60', 'week')
+    var opt = {temperature: 19.3, fromTime: '12:00', toTime: '18:60', 'days': 'week'}
+    var schedule = new Schedule(opt)
 
     assert(!schedule.isValid())
   })
 
   it('should be not valid with fromTime greater than toTime', function() {
-    var schedule = new Schedule(19.3, '23:00', '12:00', 'week')
+    var opt = {temperature: 19.3, fromTime: '23:00', toTime: '12:00', 'days': 'week'}
+    var schedule = new Schedule(opt)
 
     assert(!schedule.isValid())
   })
 
   it('should be not valid with fromTime equals toTime', function() {
-    var schedule = new Schedule(19.3, '23:00', '23:00', 'week')
+    var opt = {temperature: 19.3, fromTime: '23:00', toTime: '23:00', 'days': 'week'}
+    var schedule = new Schedule(opt)
 
     assert(!schedule.isValid())
   })
 
   it('should be not valid with days not in [week, saturday, sunday]', function() {
-    var schedule = new Schedule(19.3, '09:00', '18:00', 'inexistent')
+    var opt = {temperature: 19.3, fromTime: '09:00', toTime: '18:00', 'days': 'inexistent'}
+    var schedule = new Schedule(opt)
 
     assert(!schedule.isValid())
   })

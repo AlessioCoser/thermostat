7bac7ad388afce2f262e0d075501126112d0d84f
alessiocoser <alessio.coser@xpeppers.com>
2017-02-04 16:42:31 +0100

    if there is no current schedule thermostat use antifreeze temperature as expected

--- test/thermostat-test.js
+++ test/thermostat-test.js
@@ -7,6 +7,14 @@ const Thermostat = require('../lib/thermostat')
 
 test('Thermostat', function () {
   test('should not change state', function () {
+    test('when rele is turned on and temperature is lower than antifreeze', function() {
+      var expectedTemperature = null
+      var thermostat = new Thermostat(new Rele(1), new TemperatureSensor(4))
+
+      ok(!thermostat.shouldTurnOn(expectedTemperature))
+      ok(!thermostat.shouldTurnOff(expectedTemperature))
+    })
+
     test('when rele is turned on and temperature is lower than expected', function () {
       var expectedTemperature = 20
       var thermostat = new Thermostat(new Rele(1), new TemperatureSensor(19.2))
@@ -24,6 +32,14 @@ test('Thermostat', function () {
   })
 
   test('should I Turn On', function () {
+    test('when rele is turned off and temperature is lower than antifreeze', function() {
+      var expectedTemperature = null
+      var thermostat = new Thermostat(new Rele(0), new TemperatureSensor(4))
+
+      ok(thermostat.shouldTurnOn(expectedTemperature))
+      ok(!thermostat.shouldTurnOff(expectedTemperature))
+    })
+
     test('when rele is off and temperature is lower than expected', function () {
       var expectedTemperature = 20
       var thermostat = new Thermostat(new Rele(0), new TemperatureSensor(15))
@@ -33,6 +49,14 @@ test('Thermostat', function () {
   })
 
   test('should I Turn Off', function () {
+    test('when rele is turned on and temperature is higher than antifreeze', function() {
+      var expectedTemperature = null
+      var thermostat = new Thermostat(new Rele(1), new TemperatureSensor(6))
+
+      ok(!thermostat.shouldTurnOn(expectedTemperature))
+      ok(thermostat.shouldTurnOff(expectedTemperature))
+    })
+
     test('when rele is on and temperature is higher than expected', function () {
       var expectedTemperature = 20
       var thermostat = new Thermostat(new Rele(1), new TemperatureSensor(20))

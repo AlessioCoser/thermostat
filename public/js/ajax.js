ed06488b20ba4296cf592553f74d29c67070cb67
alessiocoser <alessio.coser@xpeppers.com>
2017-02-05 15:52:58 +0100

    add UI thermostat panel

---
+++ public/js/ajax.js
@@ -0,0 +1,17 @@
+const ajax = {
+  _hasNoErrors: function (response) {
+    if ((response.statusText === 'OK') && (!response.data.err)) {
+      return true
+    }
+    return false
+  },
+  _handleResponse: function (response) {
+    if (this._hasNoErrors(response)) {
+      return response.data.value
+    }
+  },
+  get: function (url){
+    return axios.get(url)
+    .then((response) => this._handleResponse(response))
+  }
+}
--- public/js/temperature-panel.jsx
+++ public/js/temperature-panel.jsx
@@ -1,26 +1,68 @@
-class TemperaturePanel extends React.Component {
+class ThermostatPanel extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       url: props.url,
-      temperature: '...',
-      status: null
+      status: null,
+      temperature: '.. .',
+      schedule: {}
     }
+  }
 
+  componentDidMount() {
     this.getStatus()
-    setInterval(getStatus, 5000)
+    setInterval(() => this.getStatus(), 5000)
+    this.getTemperature()
+    setInterval(() => this.getTemperature(), 300000)
+    this.getCurrentSchedule()
+    setInterval(() => this.getCurrentSchedule(), 5000)
   }
 
   getStatus() {
-    this.setState({status: this.state.status || "ciao"})
+    ajax.get('/thermostat/status')
+    .then((status) => {
+      this.setState({status: status})
+    })
+  }
+
+  getTemperature() {
+    ajax.get('/thermostat/temperature')
+    .then((temperature) => {
+      this.setState({temperature: temperature})
+    })
+  }
+
+  getCurrentSchedule() {
+    ajax.get('/schedules/current')
+    .then((schedule) => {
+      this.setState({schedule: schedule})
+    })
   }
 
   render() {
-    return <div>Stato: {this.state.status}</div>
+    var schedulePeriod = ''
+
+    if (this.state.schedule.fromTime && this.state.schedule.toTime) {
+      schedulePeriod = this.state.schedule.fromTime + " - " + this.state.schedule.toTime
+    }
+
+    return (<div class='thermostat'>
+      <div class='temperature'>
+        {this.state.temperature + ' °C'}
+      </div>
+      <div class='current-schedule'>
+        <div class='circle'>
+          <div class='icon'>{this.state.status ? 'on' : 'off'}</div>
+          <div class='text'>{this.state.status ? 'Acceso' : 'Spento'}</div>
+          <div class='scheduled-temperature'>{this.state.schedule.temperature}</div>
+          <div class='scheduled-time'>{schedulePeriod}</div>
+        </div>
+      </div>
+    </div>)
   }
 }
 
 ReactDOM.render(
-  <TemperaturePanel />,
+  <ThermostatPanel />,
   document.getElementById('temperature-panel')
 )
